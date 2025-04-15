23e62bc33f8e4f06c99b71fe9e8f6ad16b6b9ed0
Mikhail Mazurkevich <mazurkevich.mikhail.14@gmail.com>
2025-04-15 16:34:07 +0200

    Wrapping header into suspense

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/shared/components'
+import { Suspense } from 'react'
 
 export const metadata = {
 	title: 'Next.js',
@@ -13,7 +14,9 @@ export default function DashboardLayout({
 	return (
 		<html lang='en'>
 			<body>
-				<Header />
+				<Suspense>
+					<Header />
+				</Suspense>
 				{children}
 			</body>
 		</html>
